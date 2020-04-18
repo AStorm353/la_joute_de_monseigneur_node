e7c42f5b106a90485b0004a35b90f1a28b21d5bd
AStorm353 <63909666+AStorm353@users.noreply.github.com>
2020-04-18 18:38:41 +0200

    Main tournois
    main tournois

---
+++ Main.js
@@ -0,0 +1,17 @@
+class Tournois { 
+        constructor(fighter) {
+            this._combattants - [];
+            this._combattant1 = new Personnages("TheBigCobra", 18, 10, 10, 8, 0, 1 );
+            this._combattant2 = new Personnages( "Ezro", 21, 10, 8, 9, 0, 1 );
+            this._combattant3 = new Personnages( "Bigi", 24, 10, 9, 2, 0, 1 );
+            this._combattant4 = new Personnages( "Nwer", 21, 11, 10, 8, 0, 1 );
+            this._combattant5 = new Personnages( "Quantium", 16, 10, 4, 6, 0, 1 );
+            this._combattant6 = new Personnages( "Stério", 18, 10, 6, 8, 0, 1 );
+            this._combattant7 = new Personnages( "Bill", 20, 10, 10, 8, 0, 1 );
+            this._combattant8 = new Personnages( "Props", 27, 10, 9, 3, 0, 1 );
+            this._combattant9 = new Personnages( "Throcks", 23, 10, 7, 7, 0, 1 );
+            this._combattant10 = new Personnages( "Madara", 20, 10, 10, 9, 0, 1 );
+
+        }
+
+}
---
+++ Tournois.js
@@ -0,0 +1,13 @@
+class Personnages {
+    constructor(nom, âge, vie, armure, force, xp, niveau ) {
+            this._nom - nom;
+            this._âge = âge;
+            this._vie - vie;
+            this._armure - armure
+            this._force - force
+            this._xp - xp
+            this._niveau
+    }
+    
+
+}
